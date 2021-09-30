7d47b64d02efba5101456c62d49a867a77f84a14
Nikhil Sekhar <nikhilsekhar@karunya.edu.in>
2021-09-30 13:40:51 +0530

    Cleaned the comments

--- Core/Src/state_machine.c
+++ Core/Src/state_machine.c
@@ -105,30 +105,15 @@ static status Ignore_Event(StateMachine *me,Event *e)
 
 typedef status (*StateMachineAction)(StateMachine *me, Event *e);
 
-/* > Add an event called timeout, it is used very freq
- * > GETICK for HAL STM32 for return current tick.
- * > Have some interface functions
- * > The HAL func changes frm uC to uC, we need to adapt  for it.
- * >
- * */
+
 StateMachineAction const Statemachine_table[MAXSTATES][MAXEVENTS] ={
 	               /* No Event*/ 	  		/*STARTUP*/ 	 					 /*Entry*/								/*Exit*/		  			     /*PA0*/         			/* PC13*/
-/* On State */ 	 {&ON_State_Continue, 	 &Ignore_Event,      					 &ON_State_Entry,                     ON_State_Exit,					 &Ignore_Event,    			&ON_State_PC13  },
-/*Off state */	 {&OFF_State_Continue,	 &Ignore_Event,      					 &OFF_State_Entry/*offstatePA0*/,     &OFF_State_Exit,                   &OFF_State_PA0,    		&Ignore_Event  },
+/* On State */ 	 {&ON_State_Continue, 	 &Ignore_Event,      					 &ON_State_Entry,                     &ON_State_Exit,					 &Ignore_Event,    			&ON_State_PC13        },
+/*Off state */	 {&OFF_State_Continue,	 &Ignore_Event,      					 &OFF_State_Entry,                    &OFF_State_Exit,                   &OFF_State_PA0,    		&Ignore_Event         },
 /*Default state*/{&Ignore_Event, 		 &DEFAULT_State/*Default start up*/,     &Ignore_Event,    				      &Ignore_Event,					 &Ignore_Event,				&DEFAULT_State_PC13   },
 };
 
-/*pop the event
-	 * then execute below
-	 * Before calling pop() we can check the buffer length
-	 * Or if the event received is no event, don't call state machine table
-*/
-/*
- * If a state requires you to evaluate a guard condition and then move to another
- * state depending on the guard evaluation, insert the guard in the state in question
- * and avoid having an exit action (or an exit function) this would simplify the code and
- * logic further
- */
+
 
 void StateMachine_Dispatch(StateMachine * me, Event *e)
 {
@@ -153,23 +138,17 @@ void StateMachine_Dispatch(StateMachine * me, Event *e)
 
 void initstatemachine(StateMachine *me,int size_of_buffer)
 {
-	//event_typedef temp[size_of_buffer];
+
 	me->state=2;
 	me->size_of_buffer = size_of_buffer;
-	//me->event_buffer=temp;
 	me->event_buffer= (event_typedef*)malloc(size_of_buffer*sizeof(event_typedef));
-
-	//memset(me->event_buffer,NOEVENT,size_of_buffer*sizeof(event_typedef));   //seg fault  or memory is not allocated
-
 	me->event_buffer[0] = STARTUP;  //Load the first event as start up
 	me->current_buffer_length = 1;
 	me->read_index = 0;
 	me->write_index = 1;
-
 	me->ptr_Pop_Event_From_Buffer=Pop_Event_From_Buffer;
 	me->ptr_Add_Event_To_Buffer=Add_Event_To_Buffer;
 
-	//free(temp);
 	return;
 }
 
@@ -179,11 +158,7 @@ void initevent(Event *me)
 	return;
 }
 
-/*
- * 	It's better to use static allocation instead of dy. allc
- * 	memset func to set an array, memcopy to copy the memory
- *
- * 	*/
+
 //void init_buffer(StateMachine *me,int size_of_buffer){
 //	me->size_of_buffer = size_of_buffer;
 //	memset(me->event_buffer,0,size_of_buffer*sizeof(event_typedef));
